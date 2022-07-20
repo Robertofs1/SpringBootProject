5e4e8428e28b62eeaeb1adab589a0a49fe426d35
Roberto <frisancoroberto@gmail.com>
2022-07-20 17:23:06 -0300

    Service layer, component registration

--- src/main/java/com/SpringBoot/SpringBoot/resource/UserResource.java
+++ src/main/java/com/SpringBoot/SpringBoot/resource/UserResource.java
@@ -16,7 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.SpringBoot.SpringBoot.entities.User;
-import com.educandoweb.course.services.UserService;
+import com.SpringBoot.SpringBoot.services.UserService;
+
 
 @RestController
 @RequestMapping(value = "/users")
